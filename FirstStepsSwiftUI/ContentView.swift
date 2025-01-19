5c2d3c97be730693e5ab995d8808435af53e369c
Ronan Soares <soaresronansilva@gmail.com>
2025-01-19 09:18:26 -0300

    hello word in SwiftUI

---
+++ FirstStepsSwiftUI/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  FirstStepsSwiftUI
+//
+//  Created by Ronan Soares on 18/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+                
+            Image( "apple-logo-svgrepo-com")
+                .resizable()
+                .frame(width: 60, height: 60)
+            
+                Text("hello world")
+                .padding(.top , 10)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstStepsSwiftUI/FirstStepsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstStepsSwiftUIApp.swift
+//  FirstStepsSwiftUI
+//
+//  Created by Ronan Soares on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstStepsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
