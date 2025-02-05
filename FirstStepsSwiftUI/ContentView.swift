f6310bf98681c2e60ded257fb2bd0b2e98a670a8
Ronan Soares <soaresronansilva@gmail.com>
2025-02-04 22:29:35 -0300

    remocao de canvas para melhor performance

--- FirstStepsSwiftUI/ContentView.swift
+++ FirstStepsSwiftUI/ContentView.swift
@@ -10,20 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-                
+            
             Image( "apple-logo-svgrepo-com")
                 .resizable()
                 .frame(width: 60, height: 60)
             
-                Text("hello world")
+            Text("hello world")
                 .padding(.top , 10)
-                
             
-                Text("bye")
-                .padding(.top , 40)
+            Text("GoodBye")
+                .padding(.top , 10)
             
-           
-                    }
+        }
         
         
         
